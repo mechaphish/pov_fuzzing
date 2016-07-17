fba135d965149ab2f259610155fa0d7334481a56
Chris Salls <salls@cs.ucsb.edu>
2016-07-17 06:15:23 -0700

    reliability

--- main_type1.py
+++ main_type1.py
@@ -3,6 +3,8 @@ import logging
 logging.basicConfig()
 l = logging.getLogger("main_type1")
 l.setLevel("INFO")
+import tempfile
+import subprocess
 
 from farnsworth.models import Exploit, PovFuzzer1Job
 
@@ -15,6 +17,28 @@ bin_path = os.path.join(os.path.dirname(compilerex.__file__), "../bin")
 for f in os.listdir(bin_path):
     os.chmod(os.path.join(bin_path, f), 0777)
     os.chmod(os.path.join(bin_path, f), 0777)
+
+def _test_exploit(pov, binary):
+    f1 = tempfile.mktemp(suffix=".pov")
+    with open(f1, "wb") as f:
+        f.write(pov)
+    os.chmod(f1, 0777)
+    args = ["cb-test", "--negotiate", "--cb", binary, "--directory", ".", "--timeout", "3", "--should_core", "--xml", f1]
+    p = subprocess.Popen(args, stdout=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    os.remove(f1)
+
+    if any(line.startswith("not ok") for line in stdout.split("\n")):
+        return False
+
+    if any(line.startswith("ok - TYPE") for line in stdout.split("\n")):
+        return True
+    return False
+
+def _get_pov_score(fuzzer):
+    pov = fuzzer.dump_binary()
+    binary = fuzzer.binary
+    return [_test_exploit(pov, binary) for _ in range(10)].count(True) / 10.0
 
 if len(sys.argv) != 2:
     print "Usage:", "job_id"
@@ -39,10 +63,13 @@ pov_fuzzer = pov_fuzzing.Type1CrashFuzzer(cbn.path, crash=crash_payload)
 crashing_test = job.input_crash
 
 if pov_fuzzer.exploitable():
-    Exploit.create(cs=job.cs, job=job, pov_type='type1',
-                   method="fuzzer",
-                   c_code=pov_fuzzer.dump_c(),
-                   blob=pov_fuzzer.dump_binary())
+    e = Exploit.create(cs=job.cs, job=job, pov_type='type1',
+                       method="fuzzer",
+                       c_code=pov_fuzzer.dump_c(),
+                       blob=pov_fuzzer.dump_binary())
+    e.reliability = _get_pov_score(pov_fuzzer)
+    e.save()
+
     l.info("crash was able to be exploited")
 else:
     l.warning("Not exploitable")
--- main_type2.py
+++ main_type2.py
@@ -4,6 +4,8 @@ import logging
 logging.basicConfig()
 l = logging.getLogger("main_type2")
 l.setLevel("INFO")
+import tempfile
+import subprocess
 
 from farnsworth.models import Exploit, PovFuzzer2Job, Test
 
@@ -15,6 +17,29 @@ bin_path = os.path.join(os.path.dirname(compilerex.__file__), "../bin")
 for f in os.listdir(bin_path):
     os.chmod(os.path.join(bin_path, f), 0777)
     os.chmod(os.path.join(bin_path, f), 0777)
+
+def _test_exploit(pov, binary):
+    f1 = tempfile.mktemp(suffix=".pov")
+    with open(f1, "wb") as f:
+        f.write(pov)
+    os.chmod(f1, 0777)
+    args = ["cb-test", "--negotiate", "--cb", binary, "--directory", ".", "--timeout", "3", "--should_core", "--xml", f1]
+    p = subprocess.Popen(args, stdout=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    os.remove(f1)
+
+    if any(line.startswith("not ok") for line in stdout.split("\n")):
+        return False
+
+    if any(line.startswith("ok - TYPE") for line in stdout.split("\n")):
+        return True
+    return False
+
+def _get_pov_score(fuzzer):
+    pov = fuzzer.dump_binary()
+    binary = fuzzer.binary
+    return [_test_exploit(pov, binary) for _ in range(10)].count(True) / 10.0
+
 
 if len(sys.argv) != 2:
     print "Usage:", "job_id"
@@ -41,6 +66,8 @@ if pov_fuzzer.exploitable():
                    method="fuzzer",
                    c_code=pov_fuzzer.dump_c(),
                    blob=pov_fuzzer.dump_binary())
+    e.reliability = _get_pov_score(pov_fuzzer)
+    e.save()
     l.info("crash was able to be exploited")
 else:
     l.warning("Not exploitable")
