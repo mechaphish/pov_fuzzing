a181d5de1f76868a9dc9104a607e3e79c1352639
Chris Salls <salls@cs.ucsb.edu>
2016-08-01 00:30:54 -0700

    store pov test results ourself

--- main_type1.py
+++ main_type1.py
@@ -1,18 +1,19 @@
+import os
 import sys
 import logging
-logging.basicConfig()
-l = logging.getLogger("main_type1")
-l.setLevel("INFO")
 import tempfile
 import subprocess
 
-from farnsworth.models import ChallengeSetFielding, IDSRuleFielding, Exploit, PovFuzzer1Job
+from farnsworth.models import ChallengeSetFielding, IDSRuleFielding, Exploit, PovFuzzer1Job, PovTestResult
 
 import pov_fuzzing
-import os
+import compilerex
+
+logging.basicConfig()
+l = logging.getLogger("main_type1")
+l.setLevel("INFO")
 
 # make compilerex executable
-import compilerex
 bin_path = os.path.join(os.path.dirname(compilerex.__file__), "../bin")
 for f in os.listdir(bin_path):
     os.chmod(os.path.join(bin_path, f), 0777)
@@ -21,6 +22,9 @@ for f in os.listdir(bin_path):
 # make fake_single executable
 fakesingle_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin/fakesingle")
 os.chmod(fakesingle_path, 0777)
+
+
+NUM_THROWS = 5
 
 
 def _test_exploit(pov, binaries, ids_rules=None):
@@ -35,7 +39,7 @@ def _test_exploit(pov, binaries, ids_rules=None):
             f.write(ids_rules)
 
     if ids_rules is None:
-        args  = ["cb-test", "--negotiate"]
+        args = ["cb-test", "--negotiate"]
         args += ["--cb"]
         args += binaries
         args += ["--directory", ".", "--timeout", "3", "--should_core", "--xml", f1]
@@ -60,11 +64,12 @@ def _test_exploit(pov, binaries, ids_rules=None):
         return True
     return False
 
+
 def _get_pov_score(fuzzer):
     pov = fuzzer.dump_binary()
     binaries = fuzzer.binaries
     ids_rules = fuzzer.ids_rules
-    return [_test_exploit(pov, binaries, ids_rules) for _ in range(5)].count(True) / 5.0
+    return [_test_exploit(pov, binaries, ids_rules) for _ in range(NUM_THROWS)].count(True) / float(NUM_THROWS)
 
 if len(sys.argv) != 2:
     print "Usage:", "job_id"
@@ -76,6 +81,7 @@ if job is None:
     raise Exception("Couldn't find job %d", job_id)
 
 cs_fielding_id = job.payload['target_cs_fld']
+ids_fielding_id = job.payload['target_ids_fld']
 
 # get the cbnp for the originals if there isn't a fielding id
 if cs_fielding_id is None:
@@ -89,8 +95,8 @@ else:
     cbnp = map(lambda c: c.path, ChallengeSetFielding.get(id=cs_fielding_id).cbns)
     # if the ids_fielding id is not None get it
     ids_rules = None
-    if job.payload['target_ids_fld'] is not None:
-        ids_rules_obj = IDSRuleFielding.get(id=job.payload['target_ids_fld']).ids_rule
+    if ids_fielding_id is not None:
+        ids_rules_obj = IDSRuleFielding.get(id=ids_fielding_id).ids_rule
         if ids_rules_obj is not None and ids_rules_obj.rules is not None and len(str(ids_rules_obj.rules).strip()) > 0:
             ids_rules = str(ids_rules_obj.rules)
 
@@ -114,6 +120,11 @@ try:
         e.reliability = _get_pov_score(pov_fuzzer)
         # store a low reliability for the targeted ones
         if cs_fielding_id is not None:
+            # store the pov test results
+            num_success = round(e.reliability*10)
+            PovTestResult.create(exploit=e, cs_fielding_id=cs_fielding_id, ids_fielding_id=ids_fielding_id,
+                                 num_success=num_success)
+
             e.reliability = 0.01 * e.reliability
         e.save()
 
--- main_type2.py
+++ main_type2.py
@@ -1,18 +1,19 @@
 import os
 import sys
 import logging
-logging.basicConfig()
-l = logging.getLogger("main_type2")
-l.setLevel("INFO")
 import tempfile
 import subprocess
 
-from farnsworth.models import ChallengeSetFielding, IDSRuleFielding, Exploit, PovFuzzer2Job, Test
+from farnsworth.models import ChallengeSetFielding, IDSRuleFielding, Exploit, PovFuzzer2Job, Test, PovTestResult
 
+import compilerex
 import pov_fuzzing
 
+logging.basicConfig()
+l = logging.getLogger("main_type2")
+l.setLevel("INFO")
+
 # make compilerex executable
-import compilerex
 bin_path = os.path.join(os.path.dirname(compilerex.__file__), "../bin")
 for f in os.listdir(bin_path):
     os.chmod(os.path.join(bin_path, f), 0777)
@@ -21,6 +22,9 @@ for f in os.listdir(bin_path):
 # make fake_single executable
 fakesingle_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "bin/fakesingle")
 os.chmod(fakesingle_path, 0777)
+
+
+NUM_THROWS = 5
 
 
 def _test_exploit(pov, binaries, ids_rules=None):
@@ -35,7 +39,7 @@ def _test_exploit(pov, binaries, ids_rules=None):
             f.write(ids_rules)
 
     if ids_rules is None:
-        args  = ["cb-test", "--negotiate"]
+        args = ["cb-test", "--negotiate"]
         args += ["--cb"]
         args += binaries
         args += ["--directory", ".", "--timeout", "3", "--should_core", "--xml", f1]
@@ -64,7 +68,7 @@ def _get_pov_score(fuzzer):
     pov = fuzzer.dump_binary()
     binaries = fuzzer.binaries
     ids_rules = fuzzer.ids_rules
-    return [_test_exploit(pov, binaries, ids_rules) for _ in range(5)].count(True) / 5.0
+    return [_test_exploit(pov, binaries, ids_rules) for _ in range(NUM_THROWS)].count(True) / float(NUM_THROWS)
 
 
 if len(sys.argv) != 2:
@@ -77,6 +81,7 @@ if job is None:
     raise Exception("Couldn't find job %d", job_id)
 
 cs_fielding_id = job.payload['target_cs_fld']
+ids_fielding_id = job.payload['target_ids_fld']
 
 # get the cbnp for the originals if there isn't a fielding id
 if cs_fielding_id is None:
@@ -91,7 +96,7 @@ else:
     # if the ids_fielding id is not None get it
     ids_rules = None
     if job.payload['target_ids_fld'] is not None:
-        ids_rules_obj = IDSRuleFielding.get(id=job.payload['target_ids_fld']).ids_rule
+        ids_rules_obj = IDSRuleFielding.get(id=ids_fielding_id).ids_rule
         if ids_rules_obj is not None and ids_rules_obj.rules is not None and len(str(ids_rules_obj.rules).strip()) > 0:
             ids_rules = str(ids_rules_obj.rules)
 
@@ -108,21 +113,25 @@ try:
 
     if pov_fuzzer.exploitable():
         e = Exploit.create(cs=job.cs, job=job, pov_type='type2',
-                       method="fuzzer",
-                       c_code=pov_fuzzer.dump_c(),
-                       blob=pov_fuzzer.dump_binary(),
-                       crash=crashing_test)
+                           method="fuzzer",
+                           c_code=pov_fuzzer.dump_c(),
+                           blob=pov_fuzzer.dump_binary(),
+                           crash=crashing_test)
         e.reliability = _get_pov_score(pov_fuzzer)
         # store a low reliability for the targeted ones
         if cs_fielding_id is not None:
+            # store the pov test results
+            num_success = round(e.reliability*10)
+            PovTestResult.create(exploit=e, cs_fielding_id=cs_fielding_id, ids_fielding_id=ids_fielding_id,
+                                 num_success=num_success)
+
             e.reliability = 0.01 * e.reliability
         e.save()
         l.info("crash was able to be exploited")
     else:
         l.warning("Not exploitable")
 
-    if pov_fuzzer.dumpable():
-        # FIXME: we probably want to store it in a different table with custom attrs
+    if not pov_fuzzer.exploitable() and pov_fuzzer.dumpable():
         Test.create(cs=job.cs, job=job, blob=pov_fuzzer.get_leaking_payload())
         l.info("possible leaking test was created")
     else:
